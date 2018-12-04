515ec9306e0e3aff1bd03f2970f06b293c8cc33f
舒雄威 <13159166158@163.com>
2018-12-04 09:02:10 +0800

    1、修改首页的布局； 2、修改注册的逻辑。

--- MicroInsight/MIHomeViewController.m
+++ MicroInsight/MIHomeViewController.m
@@ -2,8 +2,8 @@
 //  MIHomeViewController.m
 //  MicroInsight
 //
-//  Created by Jonphy on 2018/11/19.
-//  Copyright © 2018 舒雄威. All rights reserved.
+//  Created by 舒雄威 on 2018/10/21.
+//  Copyright © 2018年 舒雄威. All rights reserved.
 //
 
 #import "MIHomeViewController.h"
@@ -14,21 +14,24 @@
 
 @interface MIHomeViewController ()
 
-@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *widthConstraint;
+@property (weak, nonatomic) IBOutlet UIButton *userBtn;
+@property (weak, nonatomic) IBOutlet UIView *albumView;
+@property (weak, nonatomic) IBOutlet UIView *communityView;
+@property (weak, nonatomic) IBOutlet UIView *loginView;
+@property (weak, nonatomic) IBOutlet UIView *shootView;
+@property (weak, nonatomic) IBOutlet UILabel *username;
 
 @end
 
 @implementation MIHomeViewController
 
-- (void)dealloc{
-    
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
     
-    self.navigationController.navigationBarHidden = YES;
-    // Do any additional setup after loading the view.
+    [self configHomeUI];
+    [self configGestureRecognizer];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -36,32 +39,65 @@
     
     NSString *username = [MILocalData getCurrentLoginUsername];
     if (![MIHelpTool isBlankString:username]) {
-        [_loginBtn setTitle:username forState:UIControlStateNormal];
+        _username.text = username;
     }
 }
 
-#pragma mark - IB
-- (IBAction)communityBtnClick:(UIButton *)sender {
+- (void)configHomeUI {
+    
+    CGFloat width = (MIScreenWidth - 5 * 3) / 3.0 * 2.0;
+    _widthConstraint.constant = width;
+    
+    _albumView.layer.cornerRadius = 3;
+    _albumView.layer.masksToBounds = YES;
+    _communityView.layer.cornerRadius = 3;
+    _communityView.layer.masksToBounds = YES;
+    _loginView.layer.cornerRadius = 3;
+    _loginView.layer.masksToBounds = YES;
+    _shootView.layer.cornerRadius = 3;
+    _shootView.layer.masksToBounds = YES;
+}
+
+- (void)configGestureRecognizer {
+    UITapGestureRecognizer *albumT = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickAlbumView:)];
+    [_albumView addGestureRecognizer:albumT];
+    
+    UITapGestureRecognizer *communityT = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickCommunityView:)];
+    [_communityView addGestureRecognizer:communityT];
+    
+    UITapGestureRecognizer *shootT = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickShootView:)];
+    [_shootView addGestureRecognizer:shootT];
+    
+    UITapGestureRecognizer *loginT = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickLoginView:)];
+    [_loginView addGestureRecognizer:loginT];
+}
+
+#pragma mark - 手势
+//相册
+- (void)clickAlbumView:(UITapGestureRecognizer *)rec {
+    UIStoryboard *board = [UIStoryboard storyboardWithName:@"MyAlbum" bundle:nil];
+    MIMyAlbumViewController *vc = [board instantiateViewControllerWithIdentifier:@"MIMyAlbumViewController"];
+    vc.albumType = MIAlbumTypePhoto;
+    [self.navigationController pushViewController:vc animated:YES];
+}
+
+//社区
+- (void)clickCommunityView:(UITapGestureRecognizer *)rec {
     UIStoryboard *board = [UIStoryboard storyboardWithName:@"Community" bundle:nil];
     MICommunityViewController *vc = [board instantiateViewControllerWithIdentifier:@"MICommunityViewController"];
     [self.navigationController pushViewController:vc animated:YES];
 }
 
-- (IBAction)photographyBtnClick:(UIButton *)sender {
+//拍摄
+- (void)clickShootView:(UITapGestureRecognizer *)rec {
     MIPhotographyViewController *vc = [[MIPhotographyViewController alloc] init];
     [self.navigationController pushViewController:vc animated:YES];
 }
 
-- (IBAction)loginBtnClick:(UIButton *)sender {
+//登录
+- (void)clickLoginView:(UITapGestureRecognizer *)rec {
     UIStoryboard *board = [UIStoryboard storyboardWithName:@"Login" bundle:nil];
     MILoginViewController *vc = [board instantiateViewControllerWithIdentifier:@"MILoginViewController"];
-    [self.navigationController pushViewController:vc animated:YES];
-}
-
-- (IBAction)myAlbumBtnClick:(UIButton *)sender {
-    UIStoryboard *board = [UIStoryboard storyboardWithName:@"MyAlbum" bundle:nil];
-    MIMyAlbumViewController *vc = [board instantiateViewControllerWithIdentifier:@"MIMyAlbumViewController"];
-    vc.albumType = MIAlbumTypePhoto;
     [self.navigationController pushViewController:vc animated:YES];
 }
 
